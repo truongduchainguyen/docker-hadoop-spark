6eae39f0440e9fbe5a0bf7dd8c6af80f7c773599
Nguyen Truong <truongduchainguyen@gmail.com>
2025-03-27 13:28:58 +0700

    feat: seperate spark and hadoop

---
+++ spark/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+TAG=0.1.0
+
+build() {
+    NAME=$1
+    IMAGE=inkeister/spark-$NAME:$TAG
+    cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
+    echo '--------------------------' building $IMAGE in $(pwd)
+    docker build -t $IMAGE .
+    cd -
+}
+
+if [ $# -eq 0 ]
+  then
+    # build base
+    build master
+    build worker
+    build history-server
+    build submit
+    build maven-template template/maven
+    build sbt-template template/sbt
+    build python-template template/python
+
+  else
+    build $1 $2
+fi
